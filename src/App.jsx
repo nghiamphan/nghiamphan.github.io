b4eeb6091eb54560bfc439120f28354564ac8f77
Nghia <19204686+nghiamphan@users.noreply.github.com>
2024-02-21 09:00:54 -0400

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { ThemeProvider, createTheme } from '@mui/material'
+import { makeStyles } from '@mui/styles'
 import ProjectList from './components/ProjectList'
 
+const theme = createTheme({
+    palette: {
+        mode:
+            window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? 'dark'
+                : 'light',
+    },
+})
+
+const useStyles = makeStyles({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.background.default,
+        },
+    },
+})
+
 const App = () => {
+    useStyles()
     return (
-        <>
-            Hello World
+        <ThemeProvider theme={theme}>
             <ProjectList />
-        </>
+        </ThemeProvider>
     )
 }
 
