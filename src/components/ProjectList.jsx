186c76ab68bbc7aa3e850e62aa85576860551827
Nghia <19204686+nghiamphan@users.noreply.github.com>
2024-02-27 22:04:48 -0400

    add self intro

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -14,6 +14,35 @@ import GitHubIcon from '@mui/icons-material/GitHub'
 import LaunchIcon from '@mui/icons-material/Launch'
 import projects from '../db/projects.json'
 
+const SelfIntroduction = () => {
+    return (
+        <Box sx={{ marginLeft: '20%', marginRight: '20%' }}>
+            <Typography sx={{ marginBottom: 2 }} variant="body1">
+                Hi, welcome to my portfolio!
+            </Typography>
+            <Typography sx={{ marginBottom: 2 }} variant="body1">
+                I leverage my expertise in Python and its powerful machine learning libraries such as
+                PyTorch and scikit-learn to design and customize intricate architectures like neural
+                networks and transformers. My passion lies in extracting actionable insights from raw data,
+                ultimately driving meaningful decisions and innovation.
+            </Typography>
+            <Typography sx={{ marginBottom: 2 }} variant="body1">
+                I also possess experience across the full software development stack, encompassing frontend
+                (React) and backend (Spring Boot, Node.js) development, along with proficiency in relational
+                (PostgreSQL, MySQL) and non-relational (MongoDB) databases.
+            </Typography>
+            <Typography sx={{ marginBottom: 5 }} variant="body1">
+                GitHub page:{' '}
+                <Link href="https://github.com/nghiamphan" target="_blank">
+                    <Tooltip title="GitHub">
+                        <GitHubIcon sx={{ verticalAlign: 'top' }} />
+                    </Tooltip>
+                </Link>
+            </Typography>
+        </Box>
+    )
+}
+
 const ProjectCard = ({ projectData }) => {
     const navigate = useNavigate()
     const { id, title, description, tools, image, github, github_2, deployment } = projectData
@@ -79,18 +108,21 @@ const ProjectList = () => {
     const evenProjects = projects.filter((_, index) => index % 2 !== 0)
 
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'row', marginTop: 15 }}>
-            <Container sx={{ display: 'flex', flexDirection: 'column' }}>
-                {oddProjects.map((data, index) => (
-                    <ProjectCard key={index} projectData={data} />
-                ))}
-            </Container>
+        <Box sx={{ marginTop: 10 }}>
+            <SelfIntroduction />
+            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                <Container sx={{ display: 'flex', flexDirection: 'column' }}>
+                    {oddProjects.map((data, index) => (
+                        <ProjectCard key={index} projectData={data} />
+                    ))}
+                </Container>
 
-            <Container sx={{ display: 'flex', flexDirection: 'column' }}>
-                {evenProjects.map((data, index) => (
-                    <ProjectCard key={index} projectData={data} />
-                ))}
-            </Container>
+                <Container sx={{ display: 'flex', flexDirection: 'column' }}>
+                    {evenProjects.map((data, index) => (
+                        <ProjectCard key={index} projectData={data} />
+                    ))}
+                </Container>
+            </Box>
         </Box>
     )
 }
